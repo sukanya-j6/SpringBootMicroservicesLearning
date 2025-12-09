81a6dd08345aaeb47875df9b0be127fe189ec865
sukanya-j6 <sukanya.jeyabal@gmail.com>
2025-12-09 15:21:54 +0530

    oracle demo

---
+++ oracle-demo/oracle-demo/src/main/java/com/example/oracle_demo/OracleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oracle_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDemoApplication.class, args);
+	}
+
+}
