15c89ddfd1e626f80b4c6e0fa80b0756d93d64b7
sukanya-j6 <sukanya.jeyabal@gmail.com>
2025-12-15 18:49:47 +0530

    lesson 7 inital commit

---
+++ security-demo-lesson7/src/main/java/com/example/security_demo_lesson7/SecurityDemoLesson7Application.java
@@ -0,0 +1,13 @@
+package com.example.security_demo_lesson7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoLesson7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoLesson7Application.class, args);
+	}
+
+}
---
+++ security-demo-lesson7/src/test/java/com/example/security_demo_lesson7/SecurityDemoLesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_demo_lesson7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoLesson7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
