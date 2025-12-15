4feff7bbd11a9bf1636677f729ece75b41ec229c
sukanya-j6 <sukanya.jeyabal@gmail.com>
2025-12-15 19:38:44 +0530

    Enable security at method level

--- security-demo-lesson7/src/main/java/com/example/security_demo_lesson7/config/SecurityConfig.java
+++ security-demo-lesson7/src/main/java/com/example/security_demo_lesson7/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security_demo_lesson7.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +10,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- security-demo-lesson7/src/main/java/com/example/security_demo_lesson7/controller/RoleController.java
+++ security-demo-lesson7/src/main/java/com/example/security_demo_lesson7/controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.example.security_demo_lesson7.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,19 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController {
 
     @RequestMapping("/")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public String index() {
         return "Hello World";
     }
 
+    // Accessible only by USER or ADMIN
     @RequestMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public String user() {
         return "user api";
     }
 
+    // Accessible only by ADMIN
     @RequestMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public String admin() {
         return "admin api";
     }
-
-
 }
